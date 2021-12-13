7685c97aa397dcf08321fdb99887746df8d0b229
krushna1911 <krushna1911@yahoo.com>
2021-12-13 03:34:39 -0500

    Comments added to the branch

---
+++ calc/calculations/division.py
@@ -0,0 +1,13 @@
+"""Division Class"""
+from calc.calculations.calculation import Calculation
+
+
+class Division(Calculation):
+    """division calculation object"""
+
+    def get_result(self):
+        """get the division results"""
+        result = self.values[0]
+        for value in self.values[1:]:
+            result = result / value
+        return round(result, 3)
